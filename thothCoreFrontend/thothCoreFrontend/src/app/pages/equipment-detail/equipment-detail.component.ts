8b49cfa1cdd8084b6fd7fd6c1aa59d8539d89291
Juan Esteban Gonzalez <juanchoes.gon@gmail.com>
2026-09-23 18:14:59 -0500

    chore: minor config adjustments

--- thothCoreFrontend/thothCoreFrontend/src/app/pages/equipment-detail/equipment-detail.component.ts
+++ thothCoreFrontend/thothCoreFrontend/src/app/pages/equipment-detail/equipment-detail.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { HttpClient } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatChipsModule } from '@angular/material/chips';
--- thothCoreFrontend/thothCoreFrontend/src/environments/environment.prod.ts
+++ thothCoreFrontend/thothCoreFrontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.thoth-core.com/api/v1'
+  apiUrl: '/api/v1'
 };
